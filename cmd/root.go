e624737aab98f3569c15b7977d2d3c9faecaa768
Selvin Ortiz <selvin@selvin.co>
2021-10-16 21:26:34 -0500

    Uncomment root cmd to run when renamer is called from the cli

--- cmd/root.go
+++ cmd/root.go
@@ -18,6 +18,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+
 	"github.com/spf13/cobra"
 
 	"github.com/spf13/viper"
@@ -28,16 +29,13 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "renamer",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Rename files in batches",
+	Long: `Rename files in batches`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello from renamer")
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
